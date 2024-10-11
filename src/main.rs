d39ffc763387edd422ba667aa4bd843b37cf8b3e
akosnad <akos@nadudvari.org>
2024-10-11 20:34:54 +0200

    feat: modem hardware init

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
 #![no_std]
 #![no_main]
 
+use alloc::vec::Vec;
 use anyhow::anyhow;
+use embedded_io::{Read, ReadReady, Write};
 use esp_backtrace as _;
 use esp_hal::{
-    clock::ClockControl, delay::Delay, gpio::Io, peripherals::Peripherals, prelude::*,
+    clock::ClockControl, delay::Delay, gpio::{Io, Level, Output}, peripherals::Peripherals, prelude::*,
     system::SystemControl, uart,
 };
 use log::info;
-use alloc::vec::Vec;
-use embedded_io::{Read, ReadReady, Write};
 
 extern crate alloc;
 use core::mem::MaybeUninit;
@@ -57,6 +57,12 @@ fn main() -> ! {
     );
     let mut psu = axp192::Axp192::new(psu_i2c);
 
+    psu.set_dcdc1_on(false).unwrap();
+    psu.set_ldo2_on(false).unwrap();
+    psu.set_ldo3_on(false).unwrap();
+    psu.set_dcdc2_on(false).unwrap();
+    psu.set_exten_on(false).unwrap();
+
     const GPS_BAUD: u32 = 9600;
     let mut gps_uart = esp_hal::uart::Uart::new_with_config(
         peripherals.UART1,
@@ -76,6 +82,28 @@ fn main() -> ! {
 
     gps_init::<_, _, GPS_BAUD>(&mut gps_uart, &delay).expect("Failed to initialize GPS");
 
+    const MODEM_BAUD: u32 = 115_200;
+    let mut modem_uart = esp_hal::uart::Uart::new_with_config(
+        peripherals.UART2,
+        uart::config::Config {
+            baudrate: MODEM_BAUD,
+            data_bits: uart::config::DataBits::DataBits8,
+            parity: uart::config::Parity::ParityNone,
+            stop_bits: uart::config::StopBits::STOP1,
+            rx_timeout: Some(50),
+            ..Default::default()
+        },
+        &clocks,
+        io.pins.gpio27,
+        io.pins.gpio26,
+    )
+    .expect("Failed to initialize modem UART");
+
+    let mut modem_pwrkey = Output::new(io.pins.gpio4, Level::Low);
+    let mut modem_power_on = Output::new(io.pins.gpio25, Level::Low);
+
+    modem_init(&mut modem_uart, &mut modem_pwrkey, &mut modem_power_on, &delay).expect("Failed to initialize modem");
+
     let gps_buf = Vec::<u8>::new();
     let mut gps_parser = ublox::Parser::new(gps_buf);
 
@@ -86,7 +114,7 @@ fn main() -> ! {
         loop {
             match it.next() {
                 Some(Ok(packet)) => {
-                    info!("GPS Packet: {:?}", packet);
+                    info!("GPS Packet: {:#?}", packet);
                 }
                 Some(Err(e)) => {
                     info!("GPS Error: {:?}", e);
@@ -100,7 +128,10 @@ fn main() -> ! {
     }
 }
 
-fn gps_init<T, M, const BAUD: u32>(gps_uart: &mut esp_hal::uart::Uart<T, M>, delay: &esp_hal::delay::Delay) -> anyhow::Result<()>
+fn gps_init<T, M, const BAUD: u32>(
+    gps_uart: &mut esp_hal::uart::Uart<T, M>,
+    delay: &esp_hal::delay::Delay,
+) -> anyhow::Result<()>
 where
     T: esp_hal::uart::Instance,
     M: esp_hal::Mode,
@@ -108,10 +139,13 @@ where
     let gps_reset_packet = ublox::CfgRstBuilder {
         nav_bbr_mask: ublox::NavBbrMask::all(),
         reset_mode: ublox::ResetMode::ControlledSoftwareReset,
-        reserved1: 0
-    }.into_packet_bytes();
+        reserved1: 0,
+    }
+    .into_packet_bytes();
     info!("Resetting GPS...");
-    gps_uart.write_all(&gps_reset_packet).map_err(|e| anyhow::anyhow!("Failed to write GPS reset packet: {:?}", e))?;
+    gps_uart
+        .write_all(&gps_reset_packet)
+        .map_err(|e| anyhow::anyhow!("Failed to write GPS reset packet: {:?}", e))?;
 
     // wait for GPS to reset
     delay.delay_millis(1000u32);
@@ -133,7 +167,9 @@ where
     }
     .into_packet_bytes();
     info!("Initializing GPS...");
-    gps_uart.write_all(&gps_init_packet).map_err(|e| anyhow!("Failed to write GPS init packet: {:?}", e))?;
+    gps_uart
+        .write_all(&gps_init_packet)
+        .map_err(|e| anyhow!("Failed to write GPS init packet: {:?}", e))?;
     gps_expect_ack(gps_uart, delay).map_err(|e| anyhow!("Failed to receive GPS ACK: {:?}", e))?;
 
     //info!("Changing GPS baud rate to {}", GPS_TARGET_BAUD);
@@ -143,30 +179,42 @@ where
         mask: ublox::CfgNav5Params::DYN,
         dyn_model: ublox::CfgNav5DynModel::Automotive,
         ..Default::default()
-    }.into_packet_bytes();
+    }
+    .into_packet_bytes();
     info!("Setting GPS to automotive mode");
-    gps_uart.write_all(&nav5_init_packet).map_err(|e| anyhow!("Failed to write NAV5 init packet: {:?}", e))?;
+    gps_uart
+        .write_all(&nav5_init_packet)
+        .map_err(|e| anyhow!("Failed to write NAV5 init packet: {:?}", e))?;
     gps_expect_ack(gps_uart, delay).map_err(|_| anyhow!("Failed to receive GPS ACK"))?;
 
     let gps_rate_init = ublox::CfgRateBuilder {
         measure_rate_ms: 500,
         nav_rate: 1,
         time_ref: ublox::AlignmentToReferenceTime::Utc,
-    }.into_packet_bytes();
+    }
+    .into_packet_bytes();
     info!("Setting GPS rate to 500ms");
-    gps_uart.write_all(&gps_rate_init).map_err(|e| anyhow!("Failed to write GPS rate init packet: {:?}", e))?;
+    gps_uart
+        .write_all(&gps_rate_init)
+        .map_err(|e| anyhow!("Failed to write GPS rate init packet: {:?}", e))?;
     gps_expect_ack(gps_uart, delay).map_err(|_| anyhow!("Failed to receive GPS ACK"))?;
 
-    let nav_pvt_rate_init = ublox::CfgMsgSinglePortBuilder::set_rate_for::<ublox::NavPvt>(1).into_packet_bytes();
+    let nav_pvt_rate_init =
+        ublox::CfgMsgSinglePortBuilder::set_rate_for::<ublox::NavPvt>(1).into_packet_bytes();
     info!("Setting NAV-PVT rate to 1 every measurement cycle");
-    gps_uart.write_all(&nav_pvt_rate_init).map_err(|e| anyhow!("Failed to write NAV-PVT rate init packet: {:?}", e))?;
+    gps_uart
+        .write_all(&nav_pvt_rate_init)
+        .map_err(|e| anyhow!("Failed to write NAV-PVT rate init packet: {:?}", e))?;
     gps_expect_ack(gps_uart, delay).map_err(|_| anyhow!("Failed to receive GPS ACK"))?;
 
     info!("GPS initialized");
     Ok(())
 }
 
-fn gps_expect_ack<T, M>(uart: &mut esp_hal::uart::Uart<T, M>, delay: &esp_hal::delay::Delay) -> anyhow::Result<()>
+fn gps_expect_ack<T, M>(
+    uart: &mut esp_hal::uart::Uart<T, M>,
+    delay: &esp_hal::delay::Delay,
+) -> anyhow::Result<()>
 where
     T: esp_hal::uart::Instance,
     M: esp_hal::Mode,
@@ -181,7 +229,9 @@ where
             anyhow::bail!("Waiting for ACK timed out");
         }
 
-        let ready = uart.read_ready().map_err(|e| anyhow!("Failed to check UART read ready: {:?}", e))?;
+        let ready = uart
+            .read_ready()
+            .map_err(|e| anyhow!("Failed to check UART read ready: {:?}", e))?;
         if !ready {
             delay.delay_millis(10u32);
             i += 1;
@@ -193,7 +243,9 @@ where
         let mut it = parser.consume(&buf[..len]);
         loop {
             match it.next() {
-                Some(Ok(ublox::PacketRef::AckAck(_))) | Some(Ok(ublox::PacketRef::MgaAck(_))) => return Ok(()),
+                Some(Ok(ublox::PacketRef::AckAck(_))) | Some(Ok(ublox::PacketRef::MgaAck(_))) => {
+                    return Ok(())
+                }
                 Some(Ok(ublox::PacketRef::AckNak(_))) => anyhow::bail!("Received NAK from GPS"),
                 Some(Err(e)) => anyhow::bail!("GPS Error: {:?}", e),
                 None => {
@@ -201,9 +253,80 @@ where
                     // not a valid ACK/NACK, continue to outer waiting loop
                     i = 0;
                     break;
-                },
+                }
                 _ => continue,
             }
+        }
+    }
+}
+
+fn modem_init<T, M, PK, PO>(
+    uart: &mut esp_hal::uart::Uart<T, M>,
+    pwrkey: &mut esp_hal::gpio::Output<PK>,
+    power_on: &mut esp_hal::gpio::Output<PO>,
+    delay: &esp_hal::delay::Delay,
+) -> anyhow::Result<()>
+where
+    T: esp_hal::uart::Instance,
+    M: esp_hal::Mode,
+    PK: esp_hal::gpio::OutputPin,
+    PO: esp_hal::gpio::OutputPin,
+{
+    info!("Resetting modem...");
+
+    power_on.set_high();
+    pwrkey.set_high();
+    delay.delay_millis(100);
+    pwrkey.set_low();
+    delay.delay_millis(1_000);
+    pwrkey.set_high();
+
+    delay.delay_millis(8_000);
+
+    info!("Modem powered on, initializing...");
+
+    uart.flush_tx().map_err(|e| anyhow!("Failed to flush modem TX: {:?}", e))?;
+
+    write!(uart, "AT\r\n").map_err(|e| anyhow!("Failed to write AT command: {:?}", e))?;
+    modem_expect_ack(uart, delay)?;
+
+    Ok(())
+}
+
+fn modem_expect_ack<T, M>(
+    uart: &mut esp_hal::uart::Uart<T, M>,
+    delay: &esp_hal::delay::Delay,
+) -> anyhow::Result<()>
+where
+    T: esp_hal::uart::Instance,
+    M: esp_hal::Mode,
+{
+    const MAX_ITERATIONS: usize = 100;
+    let mut i = 0;
+
+    loop {
+        if i >= MAX_ITERATIONS {
+            anyhow::bail!("Timed out while initializing modem");
+        }
+
+        let ready = uart.read_ready().map_err(|e| anyhow!("Failed to check UART read ready: {:?}", e))?;
+        if !ready {
+            delay.delay_millis(10);
+            i += 1;
+            continue;
+        }
+
+        let mut buf = [0u8; 256];
+        let len = uart
+            .read(&mut buf)
+            .map_err(|e| anyhow!("Failed to read modem response: {:?}", e))?;
+
+        let response = core::str::from_utf8(&buf[..len])
+            .map_err(|e| anyhow!("Failed to parse modem response: {:?}", e))?;
+        log::info!("Modem response: {}", response);
+
+        if response.contains("OK") {
+            return Ok(())
         }
     }
 }
